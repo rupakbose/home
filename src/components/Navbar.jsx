2a8455edbac206e1a2ae1178118c4877de595744
Rupak Bose <rupakbose@Rupaks-MacBook-Air.local>
2024-12-26 01:43:58 +0100

    made about me

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-
+import logo from "../assets/kevinRushLogo.png"
+import { FaLinkedin } from "react-icons/fa"
+import { FaGoogleScholar } from "react-icons/fa6"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className=" mb-20 flex items=center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+          <img src={logo} alt = "logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGoogleScholar/>
+        </div>
+    </nav>
   )
 }
 
