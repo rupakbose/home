4b03577ddb5dc54c45349a1824f23de07a37044d
Rupak Bose <rupakbose@Rupaks-MacBook-Air.local>
2024-12-26 14:18:05 +0100

    made site

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Technologies from './components/Technologies'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
         <About/>
         <Technologies/>
         <Experience/>
+        <Projects/>
+        <Contact/>
       </div>
       
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start">
                     <h1 className="pb=1 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"> Rupak Bose</h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
-                    bg-clip-text text-4xl tracking-tight text-transparent">ML Researcher</span>
+                    bg-clip-text text-3xl tracking-tight text-transparent">ML Researcher</span>
                     <p className="my-2 max-w-xl py-6 font-light tracking-tighter"> {HERO_CONTENT} </p>
                 </div>
             </div>
