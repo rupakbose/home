979b2a99c867f6da6aedc51e2a53a92fb114a780
rupakbose <boserupak1@gmail.com>
2025-04-25 18:43:16 +0530

    update consult

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                     animate="visible"
                     className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
                     bg-clip-text text-3xl tracking-tight text-transparent">
-                    ML Researcher
+                    ML Researcher | AI Consultant
                     </motion.span>
                     <motion.p variants={container(1)}
                     initial="hidden"
